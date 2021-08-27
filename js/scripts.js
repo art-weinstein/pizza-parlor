cbe80c66011ea1491eaa2ae698ff0fa8aff0704c
Art Weinstein <artur.weinstein@gmail.com>
2021-08-27 14:57:04 -0700

    place toppings into one property, have initial price show 0.00, return toFixed number to create decimals.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,22 @@
 // Business Logic
-function Pizza (size, meat, veggies){
+function Pizza (size, toppings){
   this.size = size;
-  this.meat = meat;
-  this.veggies = veggies;
+  this.toppings = toppings;
 }
 
 Pizza.prototype.price = function(){
   let pizzaSize = this.size;
-  let pizzaMeat = this.meat.length;
-  let pizzaVeggies = this.veggies.length;
+  let pizzaTopping = this.toppings.length;
+  let pizzaPrice = 0;
   if (pizzaSize === "Small"){
-    return  12 + (pizzaMeat * 2) + (pizzaVeggies * 2);
+    pizzaPrice = 12 + (pizzaTopping * 2); 
+    return  pizzaPrice;
   } else if (pizzaSize === "Medium"){
-    return 14 + (pizzaMeat * 3) + (pizzaVeggies * 3);
+    pizzaPrice = 14 + (pizzaTopping * 3); 
+    return  pizzaPrice;
   } else {
-    return 16 + (pizzaMeat * 3.5) + (pizzaVeggies * 3.5);
+    pizzaPrice = 16 + (pizzaTopping * 3.5); 
+    return  pizzaPrice;
   }
 }
 
@@ -23,19 +25,23 @@ $(document).ready(function(){
   $("form#order").submit(function(event){
     event.preventDefault();
     let size = $("#size").val();
-    let meat = [];
-    let veggies = [];
-    $("input:checkbox[name=meat]:checked").each(function(){
-      meat.push($(this).val());
+    let topping = [];
+    $("input:checkbox[name=topping]:checked").each(function(){
+      topping.push($(this).val());
     });
-  $("input:checkbox[name=veggies]:checked").each(function(){
-      veggies.push($(this).val());
-    });
-    let orderPizza = new Pizza(size, meat, veggies);
+  // $("input:checkbox[name=veggies]:checked").each(function(){
+  //     veggies.push($(this).val());
+  //   });
+    let orderPizza = new Pizza(size, topping);
     let pizzaCost = orderPizza.price();
     $(".size").text(orderPizza.size);
-    $(".meat").text(orderPizza.meat);
-    $(".veggies").text(orderPizza.veggies);
-    $(".cost").text(pizzaCost);
+    $(".toppings").text(orderPizza.toppings.join(", "));
+    // This ties into my comment in html, I was wondering if I could create a seperate list for each topping without creating new properites.
+    // $(".meat").text(orderPizza.topping);
+    // $(".veggies").text(orderPizza.topping);
+    $(".cost").text(pizzaCost.toFixed(2));
+    $(".initialCost").hide();
   });
 });
+
+
