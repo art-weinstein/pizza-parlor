24d2cd7122bdd3e1eb6920416f46a4d86b8490f8
Art Weinstein <artur.weinstein@gmail.com>
2021-08-27 12:42:06 -0700

    add size dropdown menu to HTML and add cost based on size to the pizza prototype.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,28 @@
 // Business Logic
-function Pizza (meat, veggies){
+function Pizza (size, meat, veggies){
+  this.size = size;
   this.meat = meat;
   this.veggies = veggies;
 }
 
 Pizza.prototype.price = function(){
+  let pizzaSize = this.size;
   let pizzaMeat = this.meat.length;
   let pizzaVeggies = this.veggies.length;
-  return 10 + (pizzaMeat * 2) + (pizzaVeggies * 2);
-
+  if (pizzaSize === "Small"){
+    return 12 + (pizzaMeat * 2) + (pizzaVeggies * 2);
+  } else if (pizzaSize === "Medium"){
+    return 14 + (pizzaMeat * 3) + (pizzaVeggies * 3);
+  } else {
+    return 16 + (pizzaMeat * 3.5) + (pizzaVeggies * 3.5);
+  }
 }
 
 // UI Logic
 $(document).ready(function(){
   $("form#order").submit(function(event){
     event.preventDefault();
+    const size = $("#size").val();
     let meat = [];
     let veggies = [];
     $("input:checkbox[name=meat]:checked").each(function(){
@@ -23,11 +31,10 @@ $(document).ready(function(){
   $("input:checkbox[name=veggies]:checked").each(function(){
       veggies.push($(this).val());
     });
-    let orderPizza = new Pizza(meat, veggies);
+    let orderPizza = new Pizza(size, meat, veggies);
     let pizzaCost = orderPizza.price();
     $(".meat").text(orderPizza.meat);
     $(".veggies").text(orderPizza.veggies);
     $(".cost").text(pizzaCost);
   });
 });
-
